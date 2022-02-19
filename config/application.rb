be07e9b5aad0c26506bbe38e4132f40090f750c9
remi-1201 <remi.nkjm@gmail.com>
2022-02-19 16:23:32 +0900

    rails g コマンドで自動生成しない設定など

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,14 @@ module Instigrim
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.load_defaults 5.1
+
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :feeds
   get 'sessions/new'
   resources :favorites
   resources :contacts
