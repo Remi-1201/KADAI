58e087f9f26c2933465bfb6d25d9d7ac94e8f616
remi-1201 <remi.nkjm@gmail.com>
2022-02-22 14:45:38 +0900

    config/environments/production.rb設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://nameless-caverns-85886.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'nameless-caverns-85886.herokuapp.com'}
   config.action_mailer.delivery_method = :letter_opener_web
 
   ActionMailer::Base.delivery_method = :smtp
